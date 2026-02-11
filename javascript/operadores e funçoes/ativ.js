4dd87fd3c293c792e0f45cc4ef3437fea663a2f5
vitoriapmello-lang <vitoria.p.mello@aluno.senai.br>
2026-02-11 15:33:26 -0300

    ativics

--- javascript/operadores e funçoes/ativ.js
+++ javascript/operadores e funçoes/ativ.js
@@ -9,25 +9,32 @@ function mostrarhoraAtual() {
 mostrarhoraAtual();
 
 console.log("ativ 2");
-function SomaeMedia(a, b) {
-    let soma = a + b;
+// Uma função que receba dois números e mostre: soma e média e que peça os dados para o usuário.
+function calcularSomaEMedia() {
+    let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+    let numero2 = parseFloat(prompt("Digite o segundo número:"));
+    let soma = numero1 + numero2;
     let media = soma / 2;
     console.log("soma:", soma);
     console.log("media:", media);
 }
-SomaeMedia(10, 20);
+calcularSomaEMedia();
 
 console.log("ativ 3");
-function analisaNome(nome) {
-    console.log("nome:", nome);
-    console.log("quantidade de letras:", nome.length);
-    console.log("nome em maiúsculas:", nome.toUpperCase());
+// Uma função que receba um nome e mostre: quantidade de letras e nome em maiúsculo e que peça o nome para o usuário.
+function analisarNome() {
+    let nome = prompt("Digite seu nome:");
+    console.log("Nome:", nome);
+    console.log("Quantidade de letras:", nome.length);
+    console.log("Nome em maiúsculo:", nome.toUpperCase());
 }
-analisaNome("Vitoria");
+analisarNome();
 
 console.log("ativ 4");
-function verificaFrase(frase) {
-    console.log("frase:", frase);
-    console.log("contém a palavra 'html'?", frase.includes("html"));
-}
-verificaFrase("Eu estudo html e css!");
+// Uma função que receba uma frase e verifica se contém a palavra "HTML" e que peça a frase para o usuário.
+function verificarHTML() {
+    let frase = prompt("Digite uma frase:");
+    console.log("Frase:", frase);
+    console.log("Contém 'HTML'?", frase.includes("HTML"));
+}   
+verificarHTML();
